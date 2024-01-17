8e03b097049507d7175def06bcbc6d1ac24d3e2b
asingh824 <asingh91021@gmail.com>
2024-01-16 20:48:09 -0800

    first commit

---
+++ src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
@@ -0,0 +1,25 @@
+package com.minikube.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+/**
+ * Unit test for simple App.
+ */
+public class MinikubeSampleApplicationTests {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void contextLoads() {
+    }
+}
